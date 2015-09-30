4959363e9c63244d38f94f60cabbb4d2887a5b8f
jbavari <josh@drifty.com>
2015-09-30 14:39:01 -0600

    Add click method to speaker detail to open session

--- www/app/speakerDetail/speakerDetail.js
+++ www/app/speakerDetail/speakerDetail.js
@@ -1,17 +1,20 @@
+// import {PageNavigator} from '../service/pageNavigator';
 import {IonicView, NavController, IonicApp, NavParams} from 'ionic/ionic';
+import {SessionDetailPage} from '../sessionDetail/sessionDetail';
 
 @IonicView({
   templateUrl: 'app/speakerDetail/speakerDetail.html',
 })
 
-export class SpeakerDetailPage {
+export class SpeakerDetailPage /*extends PageNavigator //couldnt get this to work, import speakerDetail in PageNavigator */ {
   constructor(nav: NavController, app: IonicApp, navParams: NavParams) {
     this.nav = nav;
     this.navParams = navParams;
     this.speaker = this.navParams.data;
   }
 
-  onInit() { 
-    console.log('SpeakerDetailPage init', this.navParams);
+  openSession(session) {
+    console.log('Open up session', session);
+    this.nav.push(SessionDetailPage, session);
   }
 }
