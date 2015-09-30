49f1955e113541a6c046830f2025350b8539ac00
jbavari <josh@drifty.com>
2015-09-30 14:38:20 -0600

    Update schedule entries to be clickable and show the session details

--- www/app/schedule/schedule.js
+++ www/app/schedule/schedule.js
@@ -1,15 +1,17 @@
 import {IonicView, NavController, IonicApp} from 'ionic/ionic';
 import {DataService} from '../service/data';
 import {DateFormat} from '../components/date-format';
-
+import {PageNavigator} from '../service/pageNavigator';
+import {SessionDetailPage} from '../sessionDetail/sessionDetail';
 @IonicView({
   templateUrl: 'app/schedule/schedule.html',
   bindings: [DataService],
   directives: [DateFormat]
 })
 
-export class SchedulePage {
+export class SchedulePage extends PageNavigator {
   constructor(nav: NavController, app: IonicApp, data: DataService) {
+    super(nav);
     this.schedule = data.getSchedule();
     this.index = 0;
     this.scheduleForTheDay = this.schedule[0];
@@ -39,5 +41,9 @@ export class SchedulePage {
     this.scheduleForTheDay = this.schedule[newIndex];
     this.index = newIndex;
     this.sessionsForTheDay = this.scheduleForTheDay.sessions;
+  }
+
+  openSession(session) {
+    this.nav.push(SessionDetailPage, session);
   }
 }
