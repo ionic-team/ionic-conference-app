b2dcd555c74dc8a8509dd824d306a9d9dd66468c
Adam Bradley <adambradley25@gmail.com>
2015-10-18 22:16:58 +0100

    remove unnecessary code

--- www/app/app.js
+++ www/app/app.js
@@ -1,26 +1,31 @@
 import {App, Platform, StatusBar} from 'ionic/ionic';
 import {DataService} from './service/data';
-import {forwardRef, FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/angular2';
-import {Http, HTTP_BINDINGS} from 'angular2/http';
-import {HomePage} from './home/home';
-import {ScheduleList} from './components/schedule-list';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {AboutPage} from './about/about';
+import {MapPage} from './map/map';
+import {SchedulePage} from './schedule/schedule';
+import {SpeakersPage} from './speakers/speakers';
+
 
 @App({
   templateUrl: 'app/app.html',
-  providers: [DataService, Http, FormBuilder, HTTP_BINDINGS],
-  directives: [forwardRef(() => ScheduleList)]
+  providers: [DataService, HTTP_PROVIDERS]
 })
-class MyApp {
+class ConferenceApp {
   constructor(platform: Platform, data: DataService) {
-    this.platform = platform;
-    data.retrieveData();
-    this.initializeApp();
-    this.rootPage = HomePage;
-  }
+    // set the root pages for each tab
+    this.tab1 = SchedulePage;
+    this.tab2 = SpeakersPage;
+    this.tab3 = MapPage;
+    this.tab4 = AboutPage;
 
-  initializeApp() {
-    this.platform.ready().then(() => {
+    // retrieve the conference data
+    data.retrieveData();
+
+    // when the platform is ready, set the status bar's style
+    platform.ready().then(() => {
       StatusBar.setStyle(StatusBar.DEFAULT);
     });
   }
+
 }
