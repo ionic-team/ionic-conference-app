b0dd810beb4ca9a53ce4d62b0b8065138930422b
jbavari <josh@drifty.com>
2015-10-17 19:19:09 -0600

    Refactor(app): Remove unused vars and remove openPage

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,4 @@
-import {App, IonicApp, Platform, StatusBar} from 'ionic/ionic';
+import {App, Platform, StatusBar} from 'ionic/ionic';
 import {DataService} from './service/data';
 import {forwardRef, FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/angular2';
 import {Http, HTTP_BINDINGS} from 'angular2/http';
@@ -11,8 +11,7 @@ import {ScheduleList} from './components/schedule-list';
   directives: [forwardRef(() => ScheduleList)]
 })
 class MyApp {
-  constructor(app: IonicApp, platform: Platform, data: DataService) {
-    this.app = app;
+  constructor(platform: Platform, data: DataService) {
     this.platform = platform;
     data.retrieveData();
     this.initializeApp();
@@ -21,30 +20,7 @@ class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // The platform is now ready. Note: if this callback fails to fire, follow
-      // the Troubleshooting guide for a number of possible solutions:
-      //
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      //
-      // First, let's hide the keyboard accessory bar (only works natively) since
-      // that's a better default:
-      //
-      // Keyboard.setAccessoryBarVisible(false);
-      //
-      // For example, we might change the StatusBar color. This one below is
-      // good for dark backgrounds and light text:
       StatusBar.setStyle(StatusBar.DEFAULT);
     });
-  }
-
-  openPage(page) {
-    // close the menu when clicking a link from the menu
-    this.app.getComponent('menu').close();
-
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    let nav = this.app.getComponent('nav');
-    nav.setRoot(page.component);
   }
 }
