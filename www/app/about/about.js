aa55638877e94d02143bdfc95afd059b44ea9045
jbavari <josh@drifty.com>
2015-10-17 19:07:28 -0600

    refactor(about): ref #25. Remove unused imports, viewBindings. Get rid of app and nav controller in constructor

--- www/app/about/about.js
+++ www/app/about/about.js
@@ -1,16 +1,15 @@
-import {NavController, Page, IonicApp} from 'ionic/ionic';
+import {Page} from 'ionic/ionic';
 import {DataService} from '../service/data';
-import {Http, HTTP_BINDINGS} from "angular2/http";
+import {Http} from "angular2/http";
 import {DateFormat} from '../components/date-format';
 
 @Page({
   templateUrl: 'app/about/about.html',
   providers: [DataService, Http],
-  viewBindings: [HTTP_BINDINGS],
   directives: [DateFormat]
 })
 export class AboutPage {
-  constructor(nav: NavController, app: IonicApp, data: DataService) {
+  constructor(data: DataService) {
     this.conferenceInfo = data.getData();
   }
 }
