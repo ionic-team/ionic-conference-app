8c71fca9dc3d736ed75eeca6045af4f9cc8e4874
Brandy Carney <brandyscarney@gmail.com>
2015-11-05 17:08:14 -0500

    update(alpha-31): updated to alpha 31 and fixed some components that changed
    references #55

--- webpack.config.js
+++ webpack.config.js
@@ -42,9 +42,9 @@ module.exports = {
   resolve: {
     modulesDirectories: [
       "node_modules",
-      "node_modules/ionic-framework/src/es5/common", // ionic-framework npm package (stable)
       "node_modules/ionic-framework/node_modules", // angular is a dependency of ionic
-      "node_modules/ionic-framework/js", // for web-animations polyfill
+      "node_modules/ionic-framework/dist/src/es5/common", // ionic-framework npm package (stable)
+      "node_modules/ionic-framework/dist/js", // for web-animations polyfill
 
       // "node_modules/ionic2/dist/src/es5/common", // driftyco/ionic2 repo (master)
       // "dist/src/es5/common",
