98ab7a96b225466c8797956456832ed3cd0dd69e
Brandy Carney <brandyscarney@gmail.com>
2015-11-06 11:10:47 -0500

    fix: updating config

---
+++ ionic.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "name": "ionic-conference-app",
+  "proxies": null,
+  "fontsOutputPath": "www/build/fonts",
+  "sassEntryFile": "/www/app/app.scss",
+  "sassOutputFile": "/www/build/css/app.css",
+  "sassIncludePaths": [
+    "node_modules/ionic-framework/dist/src/scss"
+  ],
+  "sassWatchPatterns": [
+    "www/**/*.scss"
+  ],
+  "htmlWatchPatterns": [
+    "www/**/*.html"
+  ]
+}
