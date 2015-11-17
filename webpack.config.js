dbd571156a28ef8840e7ca5b7d7a0c29e142d3cd
Brandy Carney <brandyscarney@gmail.com>
2015-11-17 10:41:22 -0500

    fix(web pack): updating web pack to match ionic-app-base

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,16 @@
 var path = require('path');
-var wwwPath = path.resolve(__dirname, 'www');
-var outputPath = path.join(wwwPath, 'build', 'js');
-var appPath = path.join(wwwPath, 'app');
-var appJsPath = path.join(appPath, 'app.js');
 
 module.exports = {
   entry: [
     "es6-shim",
-    "zone.js",
     "reflect-metadata",
-    "angular2/angular2",
-    "ionic/ionic",
     "web-animations.min",
     "moment",
-    // "./www/app/app.js"
-    appJsPath
+    "zone.js",
+    path.join(__dirname, 'www', 'app', 'app.js')
   ],
   output: {
-    path: outputPath,
+    path: path.join(__dirname, 'www', 'build', 'js'),
     filename: 'app.bundle.js'
     //pathinfo: true // show module paths in the bundle, handy for debugging
   },
@@ -35,15 +28,15 @@ module.exports = {
         include: [path.join(__dirname, 'www')],
         exclude: /node_modules/
       },
-      { 
-        // Loader to compile all of our SASS down. 
+      {
+        // Loader to compile all of our SASS down.
         // Use the `resolve-url` pipe to convert the relative url paths to something this loader
         // can work with, e.g.: url('../my-file.png') => url('/path/to/my-file.png')
         // https://github.com/bholloway/resolve-url-loader
         test: /\.scss$/,
-        loaders: ["style", "css",  "resolve-url", "sass?sourceMap"]
+        loaders: ["style", "css", "autoprefixer?browsers=last 2 version", "resolve-url", "sass?sourceMap"]
       },
-      // Any png-image or woff-font below or equal to 100K will be converted 
+      // Any png-image or woff-font below or equal to 100K will be converted
       // to inline base64 instead
       { test: /\.(png|woff|ttf)$/, loader: 'url-loader?limit=100000' }
     ]
