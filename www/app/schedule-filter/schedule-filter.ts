d1a93f0821bc5c08d68533af8bbccec068656817
Adam Bradley <adambradley25@gmail.com>
2015-12-12 14:03:29 -0600

    fix modal filter

--- www/app/schedule-filter/schedule-filter.ts
+++ www/app/schedule-filter/schedule-filter.ts
@@ -16,14 +16,15 @@ export class ScheduleFilterPage {
     this.navParams = navParams;
     this.confData = confData;
 
-    this.filteredTracks = this.navParams.data;
+    // passed in array of tracks that should be excluded (unchecked)
+    let excludeTracks = this.navParams.data;
 
     this.confData.getTracks().then(trackNames => {
 
       trackNames.forEach(trackName => {
         this.tracks.push({
           name: trackName,
-          isChecked: (this.filteredTracks.indexOf(trackName) < 0)
+          isChecked: (excludeTracks.indexOf(trackName) === -1)
         });
       });
 
@@ -38,7 +39,7 @@ export class ScheduleFilterPage {
   }
 
   applyFilters() {
-    // Pass back a new array of track names to filter by
+    // Pass back a new array of track names to exclude
     let excludeTracks = this.tracks.filter(c => !c.isChecked).map(c => c.name);
     this.close(excludeTracks);
   }
