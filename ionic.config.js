6cfa4142f38f88ea1a9fddf6dff76dc389b9d131
Tim Lancina <tim@ionic.io>
2015-12-12 15:07:05 -0600

    use local gulp

--- ionic.config.js
+++ ionic.config.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   proxies: null,
 
@@ -10,14 +9,9 @@ module.exports = {
   // ex: beforeServe, afterRun, beforePrepare, etc.
   hooks: {
     beforeServe: function(argv) {
-      var spawn = require('child_process').spawn;
-      var gulpWatch = spawn('gulp', ['watch']);
-      gulpWatch.stdout.on('data', function(data) {
-        console.log(data.toString());
-      });
-      gulpWatch.stderr.on('data', function(data) {
-        console.log('gulp watch error: ' + data.toString());
-      });
+      var path = require('path');
+      var fork = require('child_process').fork;
+      fork(path.resolve('./node_modules/.bin/gulp'), ['watch']);
     }
   },
 
