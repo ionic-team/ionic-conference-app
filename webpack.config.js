8dcfaa88f5efa374f5bfb3b9da8c9151278ebe7d
Tim Lancina <tim@ionic.io>
2015-12-22 15:11:46 -0600

    chore(): update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,18 @@
 var path = require('path');
-var paths = require('./ionic.config').paths;
 var webpack = require('webpack');
 
 
 module.exports = {
   entry: [
-    'es6-shim/es6-shim.min',
-    'zone.js/dist/zone-microtask',
+    path.normalize('es6-shim/es6-shim.min'),
     'reflect-metadata',
     'web-animations.min',
-    './app/app'
+    path.normalize('zone.js/dist/zone-microtask'),
+    path.resolve('app/app')
   ],
   output: {
-    path: path.join(__dirname, paths.wwwDir, paths.buildDir, paths.buildJSDir),
-    filename: paths.appBundle,
-    publicPath: path.join(paths.buildDir, paths.buildJSDir),
+    path: path.resolve('www/build/js'),
+    filename: 'app.bundle.js',
     pathinfo: false // show module paths in the bundle, handy for debugging
   },
   module: {
@@ -32,53 +30,26 @@ module.exports = {
           ],
           presets: ['es2015']
         },
-        include: [path.join(__dirname, paths.appDir)],
+        include: path.resolve('app'),
         exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve('node_modules/angular2'),
+        loader: 'strip-sourcemap'
       }
     ],
     noParse: [
       /es6-shim/,
       /reflect-metadata/,
       /web-animations/,
-      /zone\.js\/dist\/zone-microtask/
+      /zone\.js(\/|\\)dist(\/|\\)zone-microtask/
     ]
   },
-  plugins: [
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //     properties: true,
-    //     sequences: true,
-    //     dead_code: true,
-    //     conditionals: true,
-    //     comparisons: true,
-    //     evaluate: true,
-    //     booleans: true,
-    //     unused: true,
-    //     loops: true,
-    //     hoist_funs: true,
-    //     cascade: true,
-    //     if_return: true,
-    //     join_vars: true,
-    //     //drop_console: true,
-    //     drop_debugger: true,
-    //     unsafe: true,
-    //     hoist_vars: true,
-    //     negate_iife: true,
-    //     //side_effects: true
-    //   },
-    //   output: {
-    //     comments: false
-    //   },
-    //   verbose: true,
-    //   beautify: false,
-    //   quote_style: 3
-    // })
-  ],
   resolve: {
     alias: {
       'ionic': 'ionic-framework',
-      'web-animations.min': 'ionic-framework/js/web-animations.min',
+      'web-animations.min': path.normalize('ionic-framework/js/web-animations.min')
     },
     extensions: ['', '.js']
   }
