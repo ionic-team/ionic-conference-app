be7a95a379d47976e57aea70feab0e47de5ef6d1
Brandy Carney <brandyscarney@gmail.com>
2016-01-06 10:56:17 -0500

    fix(speaker-list): updated action sheet to latest alpha
    fixes #125

--- app/pages/speaker-list/speaker-list.js
+++ app/pages/speaker-list/speaker-list.js
@@ -8,10 +8,9 @@ import {SessionDetailPage} from '../session-detail/session-detail';
   templateUrl: 'build/pages/speaker-list/speaker-list.html'
 })
 export class SpeakerListPage {
-  constructor(nav: NavController, confData: ConferenceData, actionSheet: ActionSheet) {
+  constructor(nav: NavController, confData: ConferenceData) {
     this.nav = nav;
     this.confData = confData;
-    this.actionSheet = actionSheet;
     this.speakers = [];
 
     confData.getSpeakers().then(speakers => {
@@ -32,25 +31,31 @@ export class SpeakerListPage {
   }
 
   openSpeakerShare(speaker) {
-    this.actionSheet.open({
+    let actionSheet = ActionSheet.create({
+      title: 'Share ' + speaker.name,
       buttons: [
-        { text: 'Copy Link' },
-        { text: 'Share via ...' },
-      ],
-      titleText: 'Share ' + speaker.name,
-      cancelText: 'Cancel',
-      cancel: () => {},
-      buttonClicked: (index) => {
-        switch (index){
-          case 0 :
-            // TODO
-            return true;
-
-          case 1 :
-            // TODO
-            return true;
-        }
-      }
+        {
+          text: 'Copy Link',
+          handler: () => {
+            console.log("Copy link clicked");
+          }
+        },
+        {
+          text: 'Share via ...',
+          handler: () => {
+            console.log("Share via clicked");
+          }
+        },
+        {
+          text: 'Cancel',
+          style: 'cancel',
+          handler: () => {
+            console.log("Cancel clicked");
+          }
+        },
+      ]
     });
+
+    this.nav.present(actionSheet);
   }
 }
