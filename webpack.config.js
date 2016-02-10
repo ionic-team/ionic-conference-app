2ed59e6fd275c4616792c7b2e5aa9da4a20fb188
Tim Lancina <tim@ionic.io>
2016-02-10 10:26:41 -0600

    chore(): update ionic to alpha.57

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
   entry: [
     path.normalize('es6-shim/es6-shim.min'),
     'reflect-metadata',
-    'web-animations.min',
     path.normalize('zone.js/dist/zone-microtask'),
     path.resolve('app/app')
   ],
@@ -35,7 +34,6 @@ module.exports = {
     noParse: [
       /es6-shim/,
       /reflect-metadata/,
-      /web-animations/,
       /zone\.js(\/|\\)dist(\/|\\)zone-microtask/
     ]
   },
@@ -43,7 +41,6 @@ module.exports = {
     alias: {
       'angular2': path.resolve('node_modules/angular2'),
       'ionic': 'ionic-framework',
-      'web-animations.min': path.normalize('ionic-framework/js/web-animations.min')
     },
     extensions: ['', '.js']
   }
