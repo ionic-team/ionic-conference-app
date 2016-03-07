304e2a49693d411458bc424028e86c7d6966416e
Brandy Carney <brandyscarney@gmail.com>
2016-03-07 12:29:11 -0500

    chore(ionic): update ionic to beta.3 and add windows mode support

--- ionic.config.js
+++ ionic.config.js
@@ -7,7 +7,7 @@ module.exports = {
       dest: "www/build"
     },
     sass: {
-      src: ['app/theme/app.+(ios|md).scss'],
+      src: ['app/theme/app.+(ios|md|wp).scss'],
       dest: 'www/build/css',
       include: [
         'node_modules/ionic-angular',
