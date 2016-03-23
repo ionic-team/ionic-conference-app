34e80f25b879d8cab0e1a2971083358d3f24e13b
Tim Lancina <tim@ionic.io>
2016-03-23 09:43:34 -0500

    feat(): use Browserify by default

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,6 @@ var gulp = require('gulp'),
     argv = process.argv;
 
 /**
- * Ionic Gulp tasks, for more information on each see
- * https://github.com/driftyco/ionic-gulp-tasks
- */
-var buildWebpack = require('ionic-gulp-webpack-build');
-var buildSass = require('ionic-gulp-sass-build');
-var copyHTML = require('ionic-gulp-html-copy');
-var copyFonts = require('ionic-gulp-fonts-copy');
-
-gulp.task('watch', ['sass', 'html', 'fonts'], function(){
-  gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
-  gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
-  return buildWebpack({ watch: true });
-});
-gulp.task('build', ['sass', 'html', 'fonts'], buildWebpack);
-gulp.task('sass', buildSass);
-gulp.task('html', copyHTML);
-gulp.task('fonts', copyFonts);
-gulp.task('clean', function(done){
-  del('www/build', done);
-});
-
-/**
  * Ionic hooks
  * Add ':before' or ':after' to any Ionic project command name to run the specified
  * tasks before or after the command.
@@ -37,3 +15,34 @@ gulp.task('deploy:before', ['build']);
 // we want to 'watch' when livereloading
 var shouldWatch = argv.indexOf('-l') > -1 || argv.indexOf('--livereload') > -1;
 gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
+
+/**
+ * Ionic Gulp tasks, for more information on each see
+ * https://github.com/driftyco/ionic-gulp-tasks
+ *
+ * Using these will allow you to stay up to date if the default Ionic 2 build
+ * changes, but you are of course welcome (and encouraged) to customize your
+ * build however you see fit.
+ */
+var buildBrowserify = require('ionic-gulp-browserify-typescript');
+var buildSass = require('ionic-gulp-sass-build');
+var copyHTML = require('ionic-gulp-html-copy');
+var copyFonts = require('ionic-gulp-fonts-copy');
+var copyScripts = require('ionic-gulp-scripts-copy');
+
+gulp.task('watch', ['sass', 'html', 'fonts', 'scripts'], function(){
+  gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
+  gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
+  return buildBrowserify({ watch: true });
+});
+gulp.task('build', ['sass', 'html', 'fonts', 'scripts'], buildBrowserify);
+gulp.task('sass', buildSass);
+gulp.task('html', copyHTML);
+gulp.task('fonts', copyFonts);
+gulp.task('scripts', copyScripts);
+gulp.task('clean', function(done){
+  del('www/build', done);
+});
+
+
+
