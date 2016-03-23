ca10f9c101c849d815d51830c8368bacfef3a73b
Júlio Cotta <juliocbcotta@gmail.com>
2016-03-23 15:33:30 -0300

    includes clicker testing configuration and files for testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,43 @@
 var gulp = require('gulp'),
+    webpack = require('webpack'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
     gulpWatch = require('gulp-watch'),
     del = require('del'),
     argv = process.argv;
 
-/**
- * Ionic Gulp tasks, for more information on each see
- * https://github.com/driftyco/ionic-gulp-tasks
- */
-var buildWebpack = require('ionic-gulp-webpack-build');
-var buildSass = require('ionic-gulp-sass-build');
-var copyHTML = require('ionic-gulp-html-copy');
-var copyFonts = require('ionic-gulp-fonts-copy');
+    /**
+     * Ionic Gulp tasks, for more information on each see
+     * https://github.com/driftyco/ionic-gulp-tasks
+     */
+    var buildWebpack = require('ionic-gulp-webpack-build');
+    var buildSass = require('ionic-gulp-sass-build');
+    var copyHTML = require('ionic-gulp-html-copy');
+    var copyFonts = require('ionic-gulp-fonts-copy');
 
-gulp.task('watch', ['sass', 'html', 'fonts'], function(){
-  gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
-  gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
-  return buildWebpack({ watch: true });
-});
-gulp.task('build', ['sass', 'html', 'fonts'], buildWebpack);
-gulp.task('sass', buildSass);
-gulp.task('html', copyHTML);
-gulp.task('fonts', copyFonts);
-gulp.task('clean', function(done){
-  del('www/build', done);
-});
+    gulp.task('watch', ['sass', 'html', 'fonts'], function(){
+      gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
+      gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
+      return buildWebpack({ watch: true });
+    });
+    gulp.task('build:before', ['build']);
+    gulp.task('build', ['sass', 'html', 'fonts'], buildWebpack);
+    gulp.task('sass', buildSass);
+    gulp.task('html', copyHTML);
+    gulp.task('fonts', copyFonts);
+    gulp.task('clean', function(done){
+      del('www/build', done);
+    });
 
-/**
- * Ionic hooks
- * Add ':before' or ':after' to any Ionic project command name to run the specified
- * tasks before or after the command.
- */
-gulp.task('serve:before', ['watch']);
-gulp.task('emulate:before', ['build']);
-gulp.task('deploy:before', ['build']);
+    /**
+     * Ionic hooks
+     * Add ':before' or ':after' to any Ionic project command name to run the specified
+     * tasks before or after the command.
+     */
+    gulp.task('serve:before', ['watch']);
+    gulp.task('emulate:before', ['build']);
+    gulp.task('deploy:before', ['build']);
 
-// we want to 'watch' when livereloading
-var shouldWatch = argv.indexOf('-l') > -1 || argv.indexOf('--livereload') > -1;
-gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
+    // we want to 'watch' when livereloading
+    var shouldWatch = argv.indexOf('-l') > -1 || argv.indexOf('--livereload') > -1;
+    gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
