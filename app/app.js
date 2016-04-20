f229cfae822c63fc24489f2e4ee392500ee3e5f4
Tim Lancina <tim@ionic.io>
2016-04-20 15:45:21 -0500

    fix(): load es6-shim before angular2-polyfills
    Because es6-shim was included in the bundle it was loading after
    zone.js, which meant that Promises weren't triggering change detection
    because they were being defined after zone.js had run.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-import 'es6-shim';
 import {App, IonicApp, Events, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {ConferenceData} from './providers/conference-data';
