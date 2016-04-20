6bd4271b77715773dd8feebd0abe967f868d129b
Tim Lancina <tim@ionic.io>
2016-04-20 15:01:21 -0500

    chore(): update es6-shim typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,2 @@
-/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/googlemaps/googlemaps.d.ts" />
