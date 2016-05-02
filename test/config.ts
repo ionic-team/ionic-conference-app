710fcdcd7226829602ac943fa86a04bfd5e808a9
Julio Cotta <juliocbcotta@gmail.com>
2016-05-02 16:32:20 -0300

    removed unused files

--- test/config.ts
+++
@@ -1,5 +0,0 @@
-export const APP_DIR: string     = 'app';
-export const TEST_DIR: string    = 'test';
-export const TYPINGS_DIR: string = 'typings';
-export const DIST_DIR: string    = 'www/build';
-export const TEST_DEST: string   = `${DIST_DIR}/test`;
