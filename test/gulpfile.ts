648eb1928629378f1d48ed8223eaccdadbc6e813
Julio Cotta <juliocbcotta@gmail.com>
2016-05-02 16:27:13 -0300

    updates project to latest configs

--- test/gulpfile.ts
+++ test/gulpfile.ts
@@ -1,150 +1,100 @@
-import { APP_DIR, TEST_DIR, TEST_DEST, TYPINGS_DIR } from './config';
-import { join }          from 'path';
-import * as chalk        from 'chalk';
-import * as del          from 'del';
-import * as gulp         from 'gulp';
-import * as karma        from 'karma';
-import * as loadPlugins  from 'gulp-load-plugins';
-import * as runSequence  from 'run-sequence';
-import * as util         from 'gulp-util';
+import { join } from 'path';
 
-let plugins: any = loadPlugins();
-
-let ionicGulpfile: any = {
-  gulpfile: require(join(process.cwd(), 'gulpfile.js')),
-  logline: 'sourced Ionic\'s gulpfile @ ' + join(process.cwd(), 'gulpfile.js'),
+const config: any = {
+  gulp: require('gulp'),
+  appDir: 'app',
+  testDir: 'test',
+  testDest: 'www/build/test',
+  typingsDir: 'typings',
 };
 
-util.log(ionicGulpfile.logline);
+const imports: any = {
+  gulp: require('gulp'),
+  runSequence: require('run-sequence'),
+  ionicGulpfile: require(join(process.cwd(), 'gulpfile.js')),
+};
 
-// compile typescript into individual files, project directoy structure is replicated under www/build/test
-function buildTypescript(): any {
-  'use strict';
-
-  let tsProject: any = plugins.typescript.createProject('tsconfig.json', {
-    typescript: require('typescript'),
-  });
-  let src: Array<any> = [
-    join(TYPINGS_DIR, '/browser.d.ts'),
-    join(APP_DIR, '**/*.ts'),
-    join(TEST_DIR, '**/*.ts'),
-  ];
-  let result: any = gulp.src(src)
-    .pipe(plugins.inlineNg2Template({ base: 'www', useRelativePaths: false }))
-    .pipe(plugins.typescript(tsProject));
-
-  return result.js
-    .pipe(gulp.dest(TEST_DEST));
-}
-
-// compile E2E typescript into individual files, project directoy structure is replicated under www/build/test
-function buildE2E(): any {
-  'use strict';
-
-  let tsProject: any = plugins.typescript.createProject('tsconfig.json', {
-    typescript: require('typescript'),
-  });
-  let src: Array<any> = [
-    join(TYPINGS_DIR, '/browser.d.ts'),
-    join(APP_DIR, '**/*e2e.ts'),
-  ];
-  let result: any = gulp.src(src)
-    .pipe(plugins.typescript(tsProject));
-
-  return result.js
-    .pipe(gulp.dest(TEST_DEST));
-}
-
-// delete everything used in our test cycle here
-function clean(): any {
-  'use strict';
-
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  return del([TEST_DEST]).then((paths: Array<any>) => {
-    util.log('Deleted', chalk.yellow(paths && paths.join(', ') || '-'));
-  });
-}
-
-// run tslint against all typescript
-function lint(): any {
-  'use strict';
-
-  return gulp.src(join(APP_DIR, '**/*.ts'))
-    .pipe(plugins.tslint())
-    .pipe(plugins.tslint.report(plugins.tslintStylish, {
-      emitError: true,
-      sort: true,
-      bell: true,
-    }));
-}
-
-// run jasmine unit tests using karma with Chrome, Karma will be left open in Chrome for debug
-function debugKarma(done: Function): any {
-  'use strict';
-
-  new (<any>karma).Server({
-    configFile: join(process.cwd(), TEST_DIR, 'karma.config.js'),
-    singleRun: false,
-    browsers: ['Chrome'],
-  }).start(done);
-}
-
-// run jasmine unit tests using karma with PhantomJS2 in single run mode
-function startKarma(done: Function): any {
-  'use strict';
-
-  new (<any>karma).Server({
-    configFile: join(process.cwd(), TEST_DIR, 'karma.config.js'),
-    singleRun: true,
-  }).start(done);
-}
-
-function watchTest(): any {
-  'use strict';
-
-  plugins.watch(join(APP_DIR, '**/*.ts'), () => {
-    gulp.start('test.watch.build');
-  });
-}
-
-gulp.task('test.build.e2e', buildE2E);
-gulp.task('test.build.typescript', buildTypescript);
-gulp.task('test.clean', clean);
-gulp.task('test.karma', startKarma);
-gulp.task('test.karma.debug', debugKarma);
-gulp.task('test.lint', lint);
-gulp.task('test.watch', watchTest);
+const gulp: any = imports.gulp;
+const runSequence: any = imports.runSequence;
 
 // just a hook into ionic's build
-gulp.task('ionic.build', (done: any) => {
+gulp.task('build-app', (done: Function) => {
   runSequence(
     'build',
-    done
+    (<any>done)
   );
 });
 
-gulp.task('test.build', (done: any) => {
-  runSequence(
-    ['test.lint', 'test.clean'],
-    ['sass', 'fonts', 'html'], // these are hooks into ionic
-    'test.build.typescript',
-    done
-  );
+// compile E2E typescript into individual files, project directoy structure is replicated under www/build/test
+gulp.task('build-e2e', ['clean-test'], () => {
+  let typescript: any = require('gulp-typescript');
+  let tsProject: any = typescript.createProject('tsconfig.json');
+  let src: Array<any> = [
+    join(config.typingsDir, '/browser.d.ts'),
+    join(config.appDir, '**/*e2e.ts'),
+  ];
+  let result: any = gulp.src(src)
+    .pipe(typescript(tsProject));
+
+  return result.js
+    .pipe(gulp.dest(config.testDest));
 });
 
-// first time round we should nuke everything
-gulp.task('test.watch.build', (done: any) => {
-  runSequence(
-    'test.build',
-    'test.watch',
-    done
-  );
+// delete everything used in our test cycle here
+gulp.task('clean-test', () => {
+
+  let del: any = require('del');
+
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  return del([config.testDest]).then((paths: Array<any>) => {
+    console.log('Deleted', paths && paths.join(', ') || '-');
+  });
 });
 
-gulp.task('test', (done: any) => {
+// run jasmine unit tests using karma with PhantomJS2 in single run mode
+gulp.task('karma', (done: Function) => {
+
+  let karma: any = require('karma');
+
+  new (<any>karma).Server(
+    {
+      configFile: join(process.cwd(), config.testDir, 'karma.config.js'),
+      singleRun: true,
+    },
+    (() => done())
+  ).start();
+});
+
+// run jasmine unit tests using karma with Chrome, Karma will be left open in Chrome for debug
+gulp.task('karma-debug', (done: Function) => {
+
+  let karma: any = require('karma');
+
+  new (<any>karma).Server(
+    {
+      configFile: join(process.cwd(), config.testDir, 'karma.config.js'),
+      singleRun: false,
+      browsers: ['Chrome'],
+    },
+    (() => done())
+  ).start();
+});
+
+// run tslint against all typescript
+gulp.task('lint', () => {
+
+  let tslint: any = require('gulp-tslint');
+
+  return gulp.src(join(config.appDir, '**/*.ts'))
+    .pipe(tslint())
+    .pipe(tslint.report('verbose'));
+});
+
+// build unit tests, run unit tests, remap and report coverage
+gulp.task('unit-test', (done: Function) => {
   runSequence(
-    'test.build',
-    'test.karma',
-    done
+    ['lint', 'html'],
+    'karma',
+    (<any>done)
   );
 });
--- test/karma.config.js
+++ test/karma.config.js
@@ -1,6 +1,3 @@
-// Karma configuration
-// Generated on Wed Jul 15 2015 09:44:02 GMT+0200 (Romance Daylight Time)
-
 module.exports = function(config) {
   'use strict';
   config.set({
@@ -10,38 +7,40 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'browserify'],
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/zone.js/dist/zone-microtask.js',
-      'node_modules/zone.js/dist/long-stack-trace-zone.js',
-      'node_modules/zone.js/dist/jasmine-patch.js',
-      'node_modules/es6-module-loader/dist/es6-module-loader.js',
-      'node_modules/traceur/bin/traceur.js',
-      'node_modules/systemjs/dist/system.src.js',
-      'node_modules/reflect-metadata/Reflect.js',
-
-      { pattern: 'test/ionic-angular.js', included: false, watched: false },
-      { pattern: 'node_modules/angular2/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/ionic-angular/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
-      { pattern: 'www/build/test/**/*.js', included: false, watched: true },
-
-      'test/test-main.js'
+      'node_modules/angular2/bundles/angular2-polyfills.js', // 'Uncaught reflect-metadata shim is required when using class decorators'
+      'node_modules/traceur/bin/traceur-runtime.js',         // TypeError: undefined is not a constructor (evaluating 'new exports.Map()')
+      'app/**/*.spec.ts',
+      'typings/browser.d.ts',
+      {pattern: 'www/build/**/*.html', included: false},
     ],
 
     // list of files to exclude
     exclude: [
       'node_modules/angular2/**/*_spec.js',
-      'node_modules/ionic-angular/**/*spec*',
-      'node_modules/ionic-angular/decorators/app.js'
+      'node_modules/ionic-angular/**/*spec*'
     ],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'www/build/test/**/!(*.spec|*.stub).js': 'coverage',
+      '**/*.ts': ['browserify']
+    },
+
+    browserify: {
+      debug: true,
+      transform: [
+        ['browserify-istanbul', {
+          instrumenter: require('isparta'),
+          ignore: ['**/*.spec.ts','**/*.d.ts'],
+        }]
+      ],
+      plugin: [
+        ['tsify']
+      ]
     },
 
     // options on how to report coverage:
@@ -55,7 +54,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha','coverage'],
+    reporters: ['mocha', 'coverage'],
 
     // web server port
     port: 9876,
@@ -66,9 +65,7 @@ module.exports = function(config) {
     // GOTCHA -- Karma proxies _everything_ through base first..
     //           Also any files you want to serve need to be in the files array above with serverd: true
     proxies: {
-      // allows us to keep test code separate from app code and still have the references work
-      '/base/node_modules/ionic-angular/decorators/app.js': '/base/www/build/test/app.stub.js', // stub out Ionic's @App decorator
-      '/base/ionic-angular.js': '/base/test/ionic-angular.js'
+      '/build': '/base/www/build'
     },
 
     // level of logging
@@ -84,22 +81,12 @@ module.exports = function(config) {
       'PhantomJS',
     ],
 
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+    // https://github.com/lathonez/clicker/issues/82
+    // try increasing this value if you see the error "Disconnected (1 times), because no message in 30000 ms."
+    browserNoActivityTimeout: 30000
   });
 
-  if (process.env.APPVEYOR) {
-    config.browsers = ['IE'];
-    config.singleRun = true;
-    config.browserNoActivityTimeout = 90000; // Note: default value (10000) is not enough
-  }
-
   if (process.env.TRAVIS || process.env.CIRCLECI) {
-    config.browsers = ['Chrome_travis_ci'];
-    config.singleRun = true;
+    config.browsers = ['Chrome', 'PhantomJS'];
   }
 };
