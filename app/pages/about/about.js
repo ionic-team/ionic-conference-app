ba9004462a911eeadd12a4440d7284c2ef35433a
Brandy Carney <brandyscarney@gmail.com>
2016-05-19 14:30:05 -0400

    feat(datepicker): add datepicker to the about page
    references #54

--- app/pages/about/about.js
+++ app/pages/about/about.js
@@ -4,4 +4,6 @@ import {Page} from 'ionic-angular';
 @Page({
   templateUrl: 'build/pages/about/about.html'
 })
-export class AboutPage {}
+export class AboutPage {
+  conferenceDate = '2047-05-17';
+}
