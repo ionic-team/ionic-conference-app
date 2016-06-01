955a95a4301c55180bb0d53fc3c4b3777c527b1b
Brandy Carney <brandyscarney@gmail.com>
2016-06-01 11:40:51 -0400

    feat(popover): add popover to the about page

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,4 +1,26 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController, ViewController, Popover, Platform} from 'ionic-angular';
+
+@Page({
+  template: `
+    <ion-list [attr.no-lines]="!isIos">
+      <button ion-item (click)="close()">Learn Ionic</button>
+      <button ion-item (click)="close()">Documentation</button>
+      <button ion-item (click)="close()">Showcase</button>
+      <button ion-item (click)="close()">GitHub Repo</button>
+    </ion-list>
+  `
+})
+class PopoverPage {
+  isIos: boolean = false;
+
+  constructor(private viewCtrl: ViewController, private platform: Platform) {
+    this.isIos = platform.is('ios');
+  }
+
+  close() {
+    this.viewCtrl.dismiss();
+  }
+}
 
 
 @Page({
@@ -6,4 +28,11 @@ import {Page} from 'ionic-angular';
 })
 export class AboutPage {
   conferenceDate = '2047-05-17';
+
+  constructor(private nav: NavController) {}
+
+  presentPopover(event) {
+    let popover = Popover.create(PopoverPage);
+    this.nav.present(popover, { ev: event });
+  }
 }
