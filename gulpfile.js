5ec378dbc58c611160e3522505de4ac99a331697
Brandy Carney <brandyscarney@gmail.com>
2016-08-04 16:37:32 -0400

    build(lint): watch for changes to the typescript files and run the linter on them

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ gulp.task('watch', ['clean'], function(done){
     function(){
       gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
       gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
+      gulpWatch('app/**/*.ts', function(){ gulp.start('lint'); });
       buildBrowserify({ watch: true }).on('end', done);
     }
   );
