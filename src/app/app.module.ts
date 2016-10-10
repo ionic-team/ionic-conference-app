f5d85c0f1a3954be2341c0844b56454f2d7565d2
Justin Willis <jgw9617@gmail.com>
2016-10-10 16:45:09 -0500

    fix(): derp comma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ export const deepLinkConfig: DeepLinkConfig = {
     { component: SchedulePage, name: 'schedule', segment: 'schedule' },
     { component: MapPage, name: 'map', segment: 'map' },
     { component: SpeakerListPage, name: 'speakerList', segment: 'speaker-list' },
-    { component: AboutPage, name: 'about', segment: 'about' },,
+    { component: AboutPage, name: 'about', segment: 'about' },
     { component: Support, name: 'support', segment: 'support' },
     { component: SignupPage, name: 'signup', segment: 'signup' },
     { component: TutorialPage, name: 'tutorial', segment: 'tutorial' }
