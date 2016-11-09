3315473e7885c4aa75f4c9ec87c08beae092ee55
Justin Willis <jgw9617@gmail.com>
2016-11-09 13:31:01 -0600

    feat(maps): web or native

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -6,6 +6,7 @@ import { Platform } from 'ionic-angular';
 
 import { GoogleMap, GoogleMapsLatLng, GoogleMapsMarkerOptions } from 'ionic-native';
 
+declare var google: any;
 
 @Component({
   selector: 'page-map',
@@ -16,32 +17,66 @@ export class MapPage {
   public map: GoogleMap;
 
   constructor(public confData: ConferenceData, public platform: Platform) {
-    this.platform.ready().then(() => this.onPlatformReady());
+    // this.platform.ready().then(() => this.onPlatformReady());
   }
 
-  onPlatformReady() {
-    this.confData.getMap().subscribe(mapData => {
-      this.map = new GoogleMap('map_canvas');
+  ionViewDidLoad() {
+    if (this.platform.is('cordova') === true) {
+      let mapEle = document.getElementById('map_canvas');
+      this.confData.getMap().subscribe(mapData => {
+        this.map = new GoogleMap('map_canvas');
+        mapEle.classList.add('show-map');
 
-      GoogleMap.isAvailable().then(() => {
-        mapData.find(data => {
-          const position = new GoogleMapsLatLng(43.074395, -89.381056);
+        GoogleMap.isAvailable().then(() => {
+          mapData.find(data => {
+            const position = new GoogleMapsLatLng(43.074395, -89.381056);
 
-          this.map.animateCamera({
-            target: position,
-            zoom: 16
-          }).then(() => {
-            mapData.forEach(markerData => {
-              const markerOptions: GoogleMapsMarkerOptions = {
-                position: markerData,
-                title: markerData.name
-              };
+            this.map.animateCamera({
+              target: position,
+              zoom: 16
+            }).then(() => {
+              mapData.forEach(markerData => {
+                const markerOptions: GoogleMapsMarkerOptions = {
+                  position: markerData,
+                  title: markerData.name
+                };
 
-              this.map.addMarker(markerOptions);
+                this.map.addMarker(markerOptions);
+              });
             });
           });
         });
       });
-    });
+    } else {
+      this.confData.getMap().subscribe(mapData => {
+        let mapEle = document.getElementById('map_canvas');
+
+        let map = new google.maps.Map(mapEle, {
+          center: mapData.find(d => d.center),
+          zoom: 16
+        });
+
+        mapData.forEach(markerData => {
+          let infoWindow = new google.maps.InfoWindow({
+            content: `<h5>${markerData.name}</h5>`
+          });
+
+          let marker = new google.maps.Marker({
+            position: markerData,
+            map: map,
+            title: markerData.name
+          });
+
+          marker.addListener('click', () => {
+            infoWindow.open(map, marker);
+          });
+        });
+
+        google.maps.event.addListenerOnce(map, 'idle', () => {
+          mapEle.classList.add('show-map');
+        });
+
+      });
+    }
   }
 }
