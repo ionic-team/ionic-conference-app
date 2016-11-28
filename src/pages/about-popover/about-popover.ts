d500c8fdd219e92c061b5e14ab69cc9cfbc0f481
Justin Willis <jgw9617@gmail.com>
2016-11-28 14:55:24 -0600

    fix(popover): fix links

--- src/pages/about-popover/about-popover.ts
+++ src/pages/about-popover/about-popover.ts
@@ -8,9 +8,9 @@ import { SupportPage } from '../support/support';
 @Component({
   template: `
     <ion-list>
-      <button ion-item (click)="close('http://ionicframework.com/docs/v2/getting-started/')">Learn Ionic</button>
-      <button ion-item (click)="close('http://ionicframework.com/docs/v2/')">Documentation</button>
-      <button ion-item (click)="close('http://showcase.ionicframework.com/')">Showcase</button>
+      <button ion-item (click)="close('http://ionicframework.com/docs/v2/getting-started')">Learn Ionic</button>
+      <button ion-item (click)="close('http://ionicframework.com/docs/v2')">Documentation</button>
+      <button ion-item (click)="close('http://showcase.ionicframework.com')">Showcase</button>
       <button ion-item (click)="close('https://github.com/driftyco/ionic')">GitHub Repo</button>
       <button ion-item (click)="support()">Support</button>
     </ion-list>
