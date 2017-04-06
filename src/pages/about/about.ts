eff41c846577103224759bdc622a81ba1e51f677
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-04-06 13:38:53 -0500

    fix(about): fix popover

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
 import { PopoverController } from 'ionic-angular';
+
+import { PopoverPage } from '../about-popover/about-popover';
 
 @Component({
   selector: 'page-about',
@@ -12,7 +13,7 @@ export class AboutPage {
   constructor(public popoverCtrl: PopoverController) { }
 
   presentPopover(event: Event) {
-    let popover = this.popoverCtrl.create('PopoverPage');
+    let popover = this.popoverCtrl.create(PopoverPage);
     popover.present({ ev: event });
   }
 }
