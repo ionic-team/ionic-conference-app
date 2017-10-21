0bcb8c8490a9534473af717e2ee86058f575bdd4
Dominik Kundel <dominik.kundel@gmail.com>
2017-10-21 17:23:34 +0200

    fix(about) change routing from about to support

--- src/pages/about-popover/about-popover.ts
+++ src/pages/about-popover/about-popover.ts
@@ -24,7 +24,7 @@ export class PopoverPage {
   ) { }
 
   support() {
-    this.app.getRootNav().push('SupportPage');
+    this.app.getRootNav().setRoot('SupportPage');
     this.viewCtrl.dismiss();
   }
 
