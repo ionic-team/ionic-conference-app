2b744fc3bb64e8129b715e53e886703e3b4a8b22
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-08-16 16:29:00 +0200

    chore(): update to latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouteReuseStrategy, RouterModule, Routes } from '@angular/router';
+import { IonicRouteStrategy } from '../../node_modules/@ionic/angular';
 
 const routes: Routes = [
   {
