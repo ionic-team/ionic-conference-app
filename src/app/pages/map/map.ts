7492e19587f4756be970da27e49e291ef51e927a
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-12-11 21:54:11 +0100

    lazy maps

--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -1,12 +1,6 @@
 import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
-
 import { ConferenceData } from '../../providers/conference-data';
-
 import { Platform } from '@ionic/angular';
-
-
-declare var google: any;
-
 
 @Component({
   selector: 'page-map',
@@ -23,21 +17,22 @@ export class MapPage {
     public platform: Platform
   ) { }
 
-  ionViewDidEnter() {
+  async ionViewDidEnter() {
+    const googleMaps = await getGoogleMaps('AIzaSyB8pf6ZdFQj5qw7rc_HSGrhUwQKfIe9ICw');
     this.confData.getMap().subscribe((mapData: any) => {
       const mapEle = this.mapElement.nativeElement;
 
-      const map = new google.maps.Map(mapEle, {
+      const map = new googleMaps.Map(mapEle, {
         center: mapData.find((d: any) => d.center),
         zoom: 16
       });
 
       mapData.forEach((markerData: any) => {
-        const infoWindow = new google.maps.InfoWindow({
+        const infoWindow = new googleMaps.InfoWindow({
           content: `<h5>${markerData.name}</h5>`
         });
 
-        const marker = new google.maps.Marker({
+        const marker = new googleMaps.Marker({
           position: markerData,
           map,
           title: markerData.name
@@ -48,11 +43,33 @@ export class MapPage {
         });
       });
 
-      google.maps.event.addListenerOnce(map, 'idle', () => {
+      googleMaps.event.addListenerOnce(map, 'idle', () => {
         mapEle.classList.add('show-map');
       });
-
     });
-
   }
+}
+
+function getGoogleMaps(apiKey: string): Promise<any> {
+  const win = window as any;
+  const googleModule = win.google;
+  if (googleModule && googleModule.maps) {
+    return Promise.resolve(googleModule.maps);
+  }
+
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.31`;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    script.onload = () => {
+      const googleModule2 = win.google;
+      if (googleModule2 && googleModule2.maps) {
+        resolve(googleModule2.maps);
+      } else {
+        reject('google maps not available');
+      }
+    };
+  });
 }
