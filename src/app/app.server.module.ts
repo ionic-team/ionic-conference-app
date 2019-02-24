b9e28cab4d211155941809f2593e43a3ea7907bd
Adam Bradley <adambradley25@gmail.com>
2019-02-24 01:56:52 -0600

    add ModuleMapLoaderModule

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,11 +3,13 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    ModuleMapLoaderModule
   ],
   bootstrap: [AppComponent],
 })
