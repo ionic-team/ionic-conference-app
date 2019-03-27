de3edf0e5e995c16191dc83c7c83110754b61516
zchapple <zackarychapple30@gmail.com>
2019-03-26 17:25:03 -0700

    fix 3rd party

---
+++ src/require.config.js
@@ -0,0 +1,9 @@
+// // This file adds extra configuration for RequireJS in the
+// // devserver. For external libs, such as @ngrx, that ship with
+// // unamed AMD modules, RequireJS needs to be configured so that
+// // it can load the approriate script from the server for the
+// // external instead of having that script included in the bundle.
+// //
+// // For example, we configure RequireJS to load /store.umd.min.js
+// // from the server when it encounters require('@ngrx/store').
+// require.config({paths: {}});
