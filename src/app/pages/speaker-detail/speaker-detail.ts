3a7e41dc6eba77365615f4711bfa0c04bd973b66
Brandy Carney <brandy@ionic.io>
2019-11-11 17:41:20 -0500

    git: revert speaker detail and list

--- src/app/pages/speaker-detail/speaker-detail.ts
+++ src/app/pages/speaker-detail/speaker-detail.ts
@@ -1,10 +1,6 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ConferenceData } from '../../providers/conference-data';
-
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
-import { ActionSheetController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'page-speaker-detail',
@@ -15,10 +11,8 @@ export class SpeakerDetailPage {
   speaker: any;
 
   constructor(
-    public actionSheetCtrl: ActionSheetController,
-    public alertCtrl: AlertController,
     private dataProvider: ConferenceData,
-    public inAppBrowser: InAppBrowser,
+    private router: Router,
     private route: ActivatedRoute
   ) {}
 
@@ -34,71 +28,5 @@ export class SpeakerDetailPage {
         }
       }
     });
-  }
-
-  openSpeakerSocial(domain: string, speaker: any) {
-    this.inAppBrowser.create(
-      `https://${domain}.com/${speaker.twitter}`,
-      '_blank'
-    );
-  }
-
-  async openSpeakerShare(speaker: any) {
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: 'Share ' + speaker.name,
-      buttons: [
-        {
-          text: 'Copy Link',
-          handler: () => {
-            console.log(
-              'Copy link clicked on https://twitter.com/' + speaker.twitter
-            );
-            if (
-              (window as any)['cordova'] &&
-              (window as any)['cordova'].plugins.clipboard
-            ) {
-              (window as any)['cordova'].plugins.clipboard.copy(
-                'https://twitter.com/' + speaker.twitter
-              );
-            }
-          }
-        },
-        {
-          text: 'Share via ...'
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        }
-      ]
-    });
-
-    await actionSheet.present();
-  }
-
-  async openContact(speaker: any) {
-    const mode = 'ios'; // this.config.get('mode');
-
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: 'Contact ' + speaker.name,
-      buttons: [
-        {
-          text: `Email ( ${speaker.email} )`,
-          icon: mode !== 'ios' ? 'mail' : null,
-          handler: () => {
-            window.open('mailto:' + speaker.email);
-          }
-        },
-        {
-          text: `Call ( ${speaker.phone} )`,
-          icon: mode !== 'ios' ? 'call' : null,
-          handler: () => {
-            window.open('tel:' + speaker.phone);
-          }
-        }
-      ]
-    });
-
-    await actionSheet.present();
   }
 }
--- src/app/pages/speaker-list/speaker-list.ts
+++ src/app/pages/speaker-list/speaker-list.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 import { ConferenceData } from '../../providers/conference-data';
 
@@ -12,7 +14,9 @@ export class SpeakerListPage {
   speakers: any[] = [];
 
   constructor(
+    public actionSheetCtrl: ActionSheetController,
     public confData: ConferenceData,
+    public inAppBrowser: InAppBrowser,
     public router: Router
   ) {}
 
@@ -20,5 +24,71 @@ export class SpeakerListPage {
     this.confData.getSpeakers().subscribe((speakers: any[]) => {
       this.speakers = speakers;
     });
+  }
+
+  goToSpeakerTwitter(speaker: any) {
+    this.inAppBrowser.create(
+      `https://twitter.com/${speaker.twitter}`,
+      '_blank'
+    );
+  }
+
+  async openSpeakerShare(speaker: any) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Share ' + speaker.name,
+      buttons: [
+        {
+          text: 'Copy Link',
+          handler: () => {
+            console.log(
+              'Copy link clicked on https://twitter.com/' + speaker.twitter
+            );
+            if (
+              (window as any)['cordova'] &&
+              (window as any)['cordova'].plugins.clipboard
+            ) {
+              (window as any)['cordova'].plugins.clipboard.copy(
+                'https://twitter.com/' + speaker.twitter
+              );
+            }
+          }
+        },
+        {
+          text: 'Share via ...'
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+
+    await actionSheet.present();
+  }
+
+  async openContact(speaker: any) {
+    const mode = 'ios'; // this.config.get('mode');
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Contact ' + speaker.name,
+      buttons: [
+        {
+          text: `Email ( ${speaker.email} )`,
+          icon: mode !== 'ios' ? 'mail' : null,
+          handler: () => {
+            window.open('mailto:' + speaker.email);
+          }
+        },
+        {
+          text: `Call ( ${speaker.phone} )`,
+          icon: mode !== 'ios' ? 'call' : null,
+          handler: () => {
+            window.open('tel:' + speaker.phone);
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
   }
 }
