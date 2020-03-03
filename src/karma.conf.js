282ce7bf01b050fe9ea82d955f1afce4c1338cde
Brandy Carney <brandy@ionic.io>
2020-03-03 11:34:39 -0500

    test(e2e): add ChromeHeadless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
