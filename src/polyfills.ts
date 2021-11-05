aa76b107bd28b21d9961a2f073c0ba781ae59fbb
amandaesmith3 <amanda.s@ionic.io>
2021-11-05 12:20:41 -0500

    update Angular to v12, including auto-migration changes

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -48,7 +48,7 @@
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 // Zone JS is required by Angular itself.
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 
 // APPLICATION IMPORTS
 // ------------------------------------------------------------------------
