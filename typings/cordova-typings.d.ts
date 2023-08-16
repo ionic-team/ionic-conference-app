e8e3d9e679180959c3940ea338728e978d5f9a77
lmavonyani <lmavonyani@gmail.com>
2023-08-16 12:11:03 +0200

    Changes to version numbers

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -1,3 +1,5 @@
 
 /// <reference path="..\.vscode\typings\jquery\jquery.d.ts"/>
 /// <reference path="..\.vscode\typings\cordova-ionic\plugins\keyboard.d.ts"/>
+/// <reference path="../.vscode/typings/cordova-ionic/plugins/keyboard.d.ts"/>
+/// <reference path="../.vscode/typings/jquery/jquery.d.ts"/>
