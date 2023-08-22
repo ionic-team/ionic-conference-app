19f97e5bc2037c81ab185ac4ed525496f3e29543
Oh Martin <oh@bi-dynamics.com>
2023-08-22 19:56:31 +0200

    Updates to the Schedule

--- src/app/pages/expo-schedule/expo-schedule.module.ts
+++ src/app/pages/expo-schedule/expo-schedule.module.ts
@@ -6,6 +6,7 @@ import { IonicModule } from '@ionic/angular';
 import { ExpoSchedulePage } from './expo-schedule';
 // import { ScheduleFilterPage } from '../schedule-filter/schedule-filter';
 import { ExpoSchedulePageRoutingModule } from './expo-schedule-routing.module';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser';
 
 @NgModule({
     imports: [
@@ -14,6 +15,7 @@ import { ExpoSchedulePageRoutingModule } from './expo-schedule-routing.module';
         IonicModule,
         ExpoSchedulePageRoutingModule
     ],
+    providers: [InAppBrowser],
     declarations: [
         ExpoSchedulePage
         // ScheduleFilterPage
--- src/app/pages/expo-schedule/expo-schedule.ts
+++ src/app/pages/expo-schedule/expo-schedule.ts
@@ -1,139 +1,33 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AlertController, IonList, IonRouterOutlet, LoadingController, ModalController, ToastController, Config } from '@ionic/angular';
-
-import { ScheduleFilterPage } from '../schedule-filter/schedule-filter';
-import { ConferenceData } from '../../providers/conference-data';
-import { UserData } from '../../providers/user-data';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser';
 
 @Component({
   selector: 'page-expo-schedule',
   templateUrl: 'expo-schedule.html',
   styleUrls: ['./expo-schedule.scss'],
 })
-export class ExpoSchedulePage implements OnInit {
-  // Gets a reference to the list element
-  @ViewChild('scheduleList', { static: true }) scheduleList: IonList;
+export class ExpoSchedulePage {
 
-  ios: boolean;
-  dayIndex = 0;
-  queryText = '';
-  segment = 'all';
-  excludeTracks: any = [];
-  shownSessions: any = [];
-  groups: any = [];
-  confDate: string;
-  showSearchbar: boolean;
-
-  constructor(
-    public alertCtrl: AlertController,
-    public confData: ConferenceData,
-    public loadingCtrl: LoadingController,
-    public modalCtrl: ModalController,
-    public router: Router,
-    public routerOutlet: IonRouterOutlet,
-    public toastCtrl: ToastController,
-    public user: UserData,
-    public config: Config
-  ) { }
-
-  ngOnInit() {
-    this.updateSchedule();
-
-    this.ios = this.config.get('mode') === 'ios';
+  constructor(private iab: InAppBrowser) {
+    this.openInAppBrowser();
   }
 
-  updateSchedule() {
-    // Close any open sliding items when the schedule updates
-    if (this.scheduleList) {
-      this.scheduleList.closeSlidingItems();
-    }
-
-    this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).subscribe((data: any) => {
-      this.shownSessions = data.shownSessions;
-      this.groups = data.groups;
+  openInAppBrowser() {
+    const browser = this.iab.create('https://miningexponamibia.com/program/',
+    '_blank',
+    {
+      location: 'no',
+      hidden: 'no',
+      hardwareback: 'yes',
+      toolbar: 'no',
+      fullscreen: 'no'
     });
-  }
 
-  async presentFilter() {
-    const modal = await this.modalCtrl.create({
-      component: ScheduleFilterPage,
-      presentingElement: this.routerOutlet.nativeEl,
-      componentProps: { excludedTracks: this.excludeTracks }
-    });
-    await modal.present();
+      // browser.on('loadstop').subscribe(event => {
+        // browser.insertCSS({ code: "body{color: red;" });
+      // });
 
-    const { data } = await modal.onWillDismiss();
-    if (data) {
-      this.excludeTracks = data;
-      this.updateSchedule();
-    }
-  }
+      // browser.close();
 
-  async addFavorite(slidingItem: HTMLIonItemSlidingElement, sessionData: any) {
-    if (this.user.hasFavorite(sessionData.name)) {
-      // Prompt to remove favorite
-      this.removeFavorite(slidingItem, sessionData, 'Favorite already added');
-    } else {
-      // Add as a favorite
-      this.user.addFavorite(sessionData.name);
-
-      // Close the open item
-      slidingItem.close();
-
-      // Create a toast
-      const toast = await this.toastCtrl.create({
-        header: `${sessionData.name} was successfully added as a favorite.`,
-        duration: 3000,
-        buttons: [{
-          text: 'Close',
-          role: 'cancel'
-        }]
-      });
-
-      // Present the toast at the bottom of the page
-      await toast.present();
-    }
-
-  }
-
-  async removeFavorite(slidingItem: HTMLIonItemSlidingElement, sessionData: any, title: string) {
-    const alert = await this.alertCtrl.create({
-      header: title,
-      message: 'Would you like to remove this session from your favorites?',
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: () => {
-            // they clicked the cancel button, do not remove the session
-            // close the sliding item and hide the option buttons
-            slidingItem.close();
-          }
-        },
-        {
-          text: 'Remove',
-          handler: () => {
-            // they want to remove this session from their favorites
-            this.user.removeFavorite(sessionData.name);
-            this.updateSchedule();
-
-            // close the sliding item and hide the option buttons
-            slidingItem.close();
-          }
-        }
-      ]
-    });
-    // now present the alert on top of all other content
-    await alert.present();
-  }
-
-  async openSocial(network: string, fab: HTMLIonFabElement) {
-    const loading = await this.loadingCtrl.create({
-      message: `Posting to ${network}`,
-      duration: (Math.random() * 1000) + 500
-    });
-    await loading.present();
-    await loading.onWillDismiss();
-    fab.close();
   }
 }
