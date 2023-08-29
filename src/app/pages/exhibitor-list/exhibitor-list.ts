6df8df75f095d0252e2ec627bf980110bb7a00c0
lmavonyani <lmavonyani@gmail.com>
2023-08-29 10:34:24 +0200

    dev

--- src/app/pages/exhibitor-list/exhibitor-list.ts
+++ src/app/pages/exhibitor-list/exhibitor-list.ts
@@ -22,14 +22,27 @@ export class ExhibitorListPage {
     })
   }
 
-  async initializeItems(){
-
+  async initializeItems() Promise <any> {
+const exhibitors = await this.dataService.getExhibitors().subscribe(res => {
+  console.log(res);
+  this.exhibitors = res;
+})
 
   }
 
   async filterList(search) {
 console.log(search);
+if (!search) {
+  return;
+}
+
+this.exhibitors = this.exhibitors.filter(currentExhibitors => {
+  if (currentExhibitors.name && search) {
+    return (currentExhibitors.name.toLowerCase().indexOf(searchTerm.))
   }
+})
+
+   }
 
 
 }
