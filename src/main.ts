742985d758c7e62a650c70dcc041aa33f24cc61a
ShaneK <shane@shanessite.net>
2025-02-20 05:43:54 -0800

    feat(capacitor): Upgrade from capacitor 4 to capacitor 5

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,28 @@
-import '@angular/compiler';
+import "@angular/compiler";
 
-import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom, isDevMode } from "@angular/core";
 import {
   IonicRouteStrategy,
   provideIonicAngular,
-} from '@ionic/angular/standalone';
+} from "@ionic/angular/standalone";
 
-import { provideHttpClient } from '@angular/common/http';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from "@angular/common/http";
+import { bootstrapApplication } from "@angular/platform-browser";
 import {
   PreloadAllModules,
   provideRouter,
   RouteReuseStrategy,
   withComponentInputBinding,
   withPreloading,
-} from '@angular/router';
-import { ServiceWorkerModule, provideServiceWorker } from '@angular/service-worker';
-import { IonicStorageModule } from '@ionic/storage-angular';
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { environment } from './environments/environment';
+} from "@angular/router";
+import {
+  provideServiceWorker,
+  ServiceWorkerModule,
+} from "@angular/service-worker";
+import { IonicStorageModule } from "@ionic/storage-angular";
+import { AppComponent } from "./app/app.component";
+import { routes } from "./app/app.routes";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
@@ -37,12 +40,13 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(),
     importProvidersFrom(
       IonicStorageModule.forRoot(),
-      ServiceWorkerModule.register('ngsw-worker.js', {
+      ServiceWorkerModule.register("ngsw-worker.js", {
         enabled: environment.production,
       })
-    ), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
+    ),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
+    }),
   ],
 }).catch((err) => console.error(err));
